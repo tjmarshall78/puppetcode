64b00a7ac8b280e9d03b4a6034202dfd3e92a93b
Marshall <trmarsh@quiktrip.com>
2017-12-18 20:57:55 -0600

    base

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,4 +30,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+
+  include profile::base
 }
--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,8 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
+  user { 'hgilmore':
+      ensure => present,
+  }
+
 }
