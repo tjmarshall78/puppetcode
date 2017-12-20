b9d12d504235add5a157b2a946698995a7d68f3a
Travis Marshall <t1-trmarsh@qt.qtlab.com>
2017-12-19 19:19:26 -0600

    facts

--- site/profile/lib/facter/windowsedition.rb
+++ site/profile/lib/facter/windowsedition.rb
@@ -1,6 +1,10 @@
 Facter.add('customfact') do
   confine :osfamily => :windows
   setcode do
-    'testvalue'
+    value = nil
+    Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\QT') do |regkey|
+          value = regkey['Type']
+        end
+        value
   end
 end  
