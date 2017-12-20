294ea49aea3f0e1a74dcefb7063513fb38685468
Travis Marshall <t1-trmarsh@qt.qtlab.com>
2017-12-19 19:29:14 -0600

    rescue

--- site/profile/lib/facter/windowsedition.rb
+++ site/profile/lib/facter/windowsedition.rb
@@ -1,10 +1,14 @@
 Facter.add('QT_Type') do
   confine :osfamily => :windows
   setcode do
+   begin
     value = nil
     Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\QT') do |regkey|
           value = regkey['Type']
         end
         value
+    rescue
+      nil
+    end
   end
 end  
