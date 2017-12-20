c71a079bf4aaadb71eff4a83fee64ec75bd61458
Travis Marshall <t1-trmarsh@qt.qtlab.com>
2017-12-19 19:47:50 -0600

    iis

--- site/profile/manifests/example.pp
+++ site/profile/manifests/example.pp
@@ -1,3 +1,20 @@
 class profile::example {
+$iis_features = ['Web-WebServer','Web-Scripting-Tools']
+
+iis_feature { $iis_features:
+  ensure => 'present',
+} ->
+
+iis_site { 'minimal':
+  ensure          => 'started',
+  physicalpath    => 'c:\\inetpub\\minimal',
+  applicationpool => 'DefaultAppPool',
+  require         => File['minimal'],
+}
+
+file { 'minimal':
+  ensure => 'directory',
+  path   => 'c:\\inetpub\\minimal',
+}
 
 }
