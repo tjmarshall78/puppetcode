05b471f8e29e8741d03878f1389e15282a331e6c
Travis Marshall <t1-trmarsh@qt.qtlab.com>
2018-02-02 15:13:32 -0600

    tm

--- site/profile/manifests/example.pp
+++ site/profile/manifests/example.pp
@@ -1,5 +1,5 @@
 class profile::example {
-$iis_features = ['Web-WebServer','Web-Scripting-Tools','Web-Asp-Net45'
+$iis_features = ['Web-WebServer','Web-Scripting-Tools','Web-Asp-Net45']
 
 iis_feature { $iis_features:
   ensure => 'present',
